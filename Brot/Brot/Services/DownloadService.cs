1a9713afdad0bdb0592c3650811bc005811aa7b6
Bryan <bryanmauriciopalma@gmail.com>
2020-01-17 13:54:53 -0600

    Init on download media

---
+++ Brot/Brot/Services/DownloadService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Brot.Services
+{
+    public class DownloadService 
+    {
+        public async void DownloadImage()
+        {
+            var webClient = new WebClient();
+            webClient.DownloadDataCompleted += (s, e) => {
+                var bytes = e.Result; // get the downloaded data
+                string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                string localFilename = "downloaded.png";
+                string localPath = Path.Combine(documentsPath, localFilename);
+                File.WriteAllBytes(localPath, bytes); // writes to local storage
+            };
+            var url = new Uri("https://www.xamarin.com/content/images/pages/branding/assets/xamagon.png");
+            webClient.DownloadDataAsync(url);
+        }
+    }
+}
