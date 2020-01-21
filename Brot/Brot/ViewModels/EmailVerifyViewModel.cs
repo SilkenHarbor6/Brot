f12e856e49d19023d0a226b0c6d6238415cfdded
Byron Jimenez <byronjimenez9911@gmail.com>
2020-01-20 21:51:53 -0600

    AuthCode & verifyEmail interface

--- Brot/Brot/ViewModels/EmailVerifyViewModel.cs
+++ Brot/Brot/ViewModels/EmailVerifyViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Brot.Models;
 using Brot.Services;
+using Brot.Views;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,6 +13,7 @@ namespace Brot.ViewModels
         private bool _emailverify;
         private bool _sendcode;
         private string _email;
+        private string _code;
 
         public bool EmailVerify
         {
@@ -30,21 +32,35 @@ namespace Brot.ViewModels
             set { SetProperty(ref _email, value); }
         }
 
+        public string Code
+        {
+            get { return _code; }
+            set { SetProperty(ref _code, value); }
+        }
+
         public EmailVerifyViewModel()
         {
             EmailVerify = true;
             SendCode = false;
         }
 
+        public ICommand Verify
+        {
+            get
+            {
+                return new Xamarin.Forms.Command(VerifyEmail);
+            }
+        }
+
         public ICommand Send
         {
             get
             {
-                return new Xamarin.Forms.Command(SendCod);
+                return new Xamarin.Forms.Command(SenCode);
             }
         }
 
-        private async void SendCod(object obj)
+        private async void VerifyEmail(object obj)
         {
             if (string.IsNullOrEmpty(Email))
             {
@@ -61,6 +77,26 @@ namespace Brot.ViewModels
                 else
                 {
                     await App.Current.MainPage.DisplayAlert("Error", resp.Message, "Ok");
+                }
+            }  
+        }
+
+        private async void SenCode(object obj)
+        {
+            if (string.IsNullOrEmpty(Code))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "Ingresa tu codigo", "Ok");
+            }
+            else
+            {
+                var resp = await RestClient.Post<userModel>("users/authcode/"+ Code, null);
+                if (resp.IsSuccess)
+                {
+                    await App.Current.MainPage.Navigation.PushAsync(new RecoveryPass());
+                }
+                else
+                {
+                    await App.Current.MainPage.DisplayAlert("Error", "Ocurrió un problema", "Ok");
                 }
             }
         }
---
+++ Brot/Brot/ViewModels/RecoveryPassViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Brot.Models;
+using Brot.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Brot.ViewModels
+{
+   public class RecoveryPassViewModel : BaseViewModel
+    {
+        private String np;
+        private String rp;
+        public String newPass
+        {
+            get { return np; }
+            set { SetProperty(ref np, value); }
+        }
+        public String repeatedPass
+        {
+            get { return rp; }
+            set { SetProperty(ref rp, value); }
+        }
+        public ICommand passwordCommand
+        {
+            get
+            {
+                return new Xamarin.Forms.Command(recoveryPass);
+            }
+        }
+
+        private async void recoveryPass()
+        {
+            IsRefreshing = true;
+
+            if (np != rp)
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "Las claves no coinciden", "Aceptar");
+                IsRefreshing = false;
+                return;
+            }
+            userModel u = new userModel();
+            u.id_user = Singleton.Instance.User.id_user;
+            u.pass = np;
+            var resp = await RestClient.Put<userModel>("users/pass", op, u);
+            if (!resp)
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "La clave antigua es incorrecta, por favor revisela", "Aceptar");
+                IsRefreshing = false;
+                return;
+            }
+            await App.Current.MainPage.DisplayAlert("", "La clave ha sido cambiada exitosamente", "Aceptar");
+            IsRefreshing = false;
+            await App.Current.MainPage.Navigation.PopAsync();
+        }
+    }
+}
